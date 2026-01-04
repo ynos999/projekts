1ad72909d4b9958bc301bd3b5daf715e60979995
Ed <ynos21@inbox.lv>
2026-01-04 19:06:29 +0200

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views  # Importējam visu views moduli uzreiz
+from . import views  # Importējam visu moduli, lai novērstu ciklus
 
 app_name = 'accounts'
 
@@ -14,7 +14,7 @@ urlpatterns = [
     path('search/', views.global_search, name='global-search'),
     path('password-change/', views.PasswordChangeView.as_view(), name='password-change'),
     
-    # Izmantojam tiešo norādi uz klasi caur 'views' moduli
+    # ŠĪ RINDIŅA IR SVARĪGĀKĀ:
     path('login/', views.MyLoginView.as_view(), name='login'),
     
     path('logout/', auth_views.LogoutView.as_view(next_page='accounts:login'), name='logout'),
